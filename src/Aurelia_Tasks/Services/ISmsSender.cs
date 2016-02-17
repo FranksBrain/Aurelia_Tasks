2f6c33ef75f58190fe459f39cd99e7b5f797ab8a
FranksBrain <frank@franksbrain.com>
2016-02-17 10:55:29 -0500

    Initial commit

---
+++ src/Aurelia_Tasks/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aurelia_Tasks.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
---
+++ src/Aurelia_Tasks/wwwroot/app.js
@@ -0,0 +1,7 @@
+var App = (function () {
+    function App() {
+        this.message = "Hello from Aureeeeeelia!";
+    }
+    return App;
+})();
+exports.App = App;
